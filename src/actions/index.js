ad246da37f23736fbaa49a8f1b8df3e219de9796
Fayjus Salehin <fsalehin@live.com>
2018-01-21 16:24:11 +0200

    Final update

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'e2d8e4cd9735ede4b7d0ec08433b5fa4';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
